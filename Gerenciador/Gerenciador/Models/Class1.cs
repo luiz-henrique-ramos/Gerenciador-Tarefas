579415c4b8490625ecdfe7d7499ba80cae6c812e
Luiz Henrique <lhenriqueramos020@email.com>
2023-10-24 01:39:00 -0300

    Primeiro commit

---
+++ Gerenciador/Gerenciador/Models/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciador_Tarefas.models
+{
+    class Gerenciador
+    {
+        private List<string> tarefas = new List<string>();
+
+        public Gerenciador()
+        {
+
+        }
+
+        public void AdicionarTarefa(string data, string tarefa)
+        {
+            int numero = tarefas.Count + 1;
+            tarefas.Add(numero + " ==> Dia: " + data + " - " + tarefa);
+        }
+
+        public List<string> ObterTarefas()
+        {
+            return tarefas;
+        }
+
+        public void ApagarTarefa(int numero)
+        {
+            if (numero >= 1 && numero <= tarefas.Count)
+            {
+                tarefas.RemoveAt(numero - 1);
+                RenumerarTarefas();
+            }
+            else
+            {
+                Console.WriteLine("Tarefa não encontrada");
+            }
+        }
+
+        private void RenumerarTarefas()
+        {
+            for (int i = 0; i < tarefas.Count; i++)
+            {
+                string tarefa = tarefas[i].Substring(tarefas[i].IndexOf(" ") + 1);
+                tarefas[i] = $"{i + 1} {tarefa}";
+            }
+        }
+    }
+}
---
+++ Gerenciador/Gerenciador/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Gerenciador_Tarefas.models;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine("Olá, Bem Vindo ao Gerenciador de Tarefas!!");
+        Console.WriteLine("------------------------------------------");
+
+        Gerenciador gerenciador = new Gerenciador();
+
+        bool Ativo = true;
+
+        while (Ativo == true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Digite a opção desejada:");
+            Console.WriteLine("1- Adicionar tarefa");
+            Console.WriteLine("2- Ver a Lista de tarefas");
+            Console.WriteLine("3- Apagar tarefa");
+            Console.WriteLine("4- Encerrar o programa");
+            Console.WriteLine();
+
+            string escolha = Console.ReadLine();
+
+            switch (escolha)
+            {
+                case "1":
+                    Console.WriteLine("Data:");
+                    string Dia = Console.ReadLine();
+                    Console.WriteLine("Tarefa:");
+                    string Tarefa = Console.ReadLine();
+
+                    gerenciador.AdicionarTarefa(Dia, Tarefa);
+
+                    Console.WriteLine("Tarefa adicionada!!");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Lista de Tarefas:");
+                    var tarefas = gerenciador.ObterTarefas();
+                    foreach (var tarefa in tarefas)
+                    {
+                        Console.WriteLine(tarefa);
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine("Digite o número da tarefa a ser apagada");
+                    string numero = Console.ReadLine();
+                    if (int.TryParse(numero, out int numeroReal))
+                    {
+                        gerenciador.ApagarTarefa(numeroReal);
+                        Console.WriteLine("Tarefa Apagada!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Número inválido. Tente novamente.");
+                    }
+                    break;
+
+                case "4":
+                    Ativo = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
